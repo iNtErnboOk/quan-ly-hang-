5222582be819a036bab50917d6135900e8b7f567
internbook <conlada123@gmail.com>
2025-12-18 23:10:35 +0700

    Delete CNPM directory

--- CNPM/CNPM2/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('add/', views.add_item, name='add_item'),
-    path('delete/<int:pk>/', views.delete_item, name='delete_item'),
-    path('update/<int:pk>/', views.update_item, name='update_item'),
-]
